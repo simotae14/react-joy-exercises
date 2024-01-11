a6e5ebbbcd79182a8848957f60c6d38a58060274
Simone Taeggi <startae14@gmail.com>
2024-01-12 00:46:28 +0100

    02 Build Your Own React

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
-// 1. Import dependencies
-import React from 'react';
-import { createRoot } from 'react-dom/client';
+function render(reactElement, containerDOMElement) {
+  /* Your code here! */
+  // 1. Create a DOM element
+  const domElement = document.createElement(reactElement.type);
 
-// 2. Create a React element
-const element = React.createElement(
-  'p',
-  { id: 'hello' },
-  'Hello World!'
-);
+  // 2. Update the properties of the element
+  domElement.innerText = reactElement.children;
+  // 3. Add the attributes
+  for (const key in reactElement.props) {
+    const value = reactElement.props[key];
+    domElement.setAttribute(key, value);
+  }
 
-// 3. Render the application
-const container = document.querySelector('#root');
-const root = createRoot(container);
-root.render(element);
+  // 4. put the new element inside the container
+  containerDOMElement.appendChild(domElement);
+}
+
+const reactElement = {
+  type: 'a',
+  props: {
+    href: 'https://wikipedia.org/',
+  },
+  children: 'Read more on Wikipedia',
+};
+
+const containerDOMElement =
+  document.querySelector('#root');
+
+render(reactElement, containerDOMElement);
