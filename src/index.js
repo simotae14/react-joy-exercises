090ba5ffd8f674ba848811eb1bee56c2b3ad5644
Simone Taeggi <startae14@gmail.com>
2024-01-12 00:48:45 +0100

    03 Understanding JSX

--- src/index.js
+++ src/index.js
@@ -1,29 +1,22 @@
-function render(reactElement, containerDOMElement) {
-  /* Your code here! */
-  // 1. Create a DOM element
-  const domElement = document.createElement(reactElement.type);
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 
-  // 2. Update the properties of the element
-  domElement.innerText = reactElement.children;
-  // 3. Add the attributes
-  for (const key in reactElement.props) {
-    const value = reactElement.props[key];
-    domElement.setAttribute(key, value);
-  }
+//// Old way:
+// const element = React.createElement(
+//   'p',
+//   {
+//     id: 'hello',
+//   },
+//   'Hello World!'
+// );
 
-  // 4. put the new element inside the container
-  containerDOMElement.appendChild(domElement);
-}
+// New way:
+const element = (
+  <p id="hello">
+    Hello World!
+  </p>
+);
 
-const reactElement = {
-  type: 'a',
-  props: {
-    href: 'https://wikipedia.org/',
-  },
-  children: 'Read more on Wikipedia',
-};
-
-const containerDOMElement =
-  document.querySelector('#root');
-
-render(reactElement, containerDOMElement);
+const container = document.querySelector('#root');
+const root = createRoot(container);
+root.render(element);
