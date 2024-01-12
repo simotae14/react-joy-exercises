97d817a26a66ffda6a9df535ab5b94b01489fa91
Simone Taeggi <startae14@gmail.com>
2024-01-12 01:25:51 +0100

    11 Log an element Component

--- src/index.js
+++ src/index.js
@@ -1,28 +1,9 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
 
-function FriendlyGreeting({ name }) {
-  return (
-    <p
-      style={{
-        fontSize: '1.25rem',
-        textAlign: 'center',
-        color: 'sienna',
-      }}
-    >
-      Greetings, {name}!
-    </p>
-  );
-}
-
-// The video was filmed using React 17, and so
-// this playground has been updated for React 18.
-const root = createRoot(document.querySelector('#root'));
-
-root.render(
+const element = (
   <div>
-    <FriendlyGreeting name="Josh" />
-    <FriendlyGreeting name="Anita" />
-    <FriendlyGreeting name="Rahul" />
+    Hello World
   </div>
 );
+
+console.log(element);
