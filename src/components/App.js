19e39e1c0f9e7c80ce999cf3ff60eebc5962adaa
Simone Taeggi <startae14@gmail.com>
2024-01-12 23:09:36 +0100

    15 Application Structure

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import Header from './Header';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <p>Typical app structure</p>
+      </main>
+    </>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,31 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
+import App from './components/App';
+
 const root = createRoot(
   document.querySelector('#root')
 );
 
-function Button({ children, color }) {
-  return (
-    <button
-      style={{
-        border: '2px solid',
-        color:  color,
-        borderColor: color,
-        background: 'white',
-        borderRadius: 4,
-        padding: 16,
-        margin: 8,
-      }}
-    >
-      {children}
-    </button>
-  )
-}
-
-root.render(
-  <div>
-    <Button color="red">Cancel</Button>
-    <Button color="black">Confirm</Button>
-  </div>
-);
+root.render(<App />);
