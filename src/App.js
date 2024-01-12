ee67579ce24c7463b95179b3a0e3bb0119955b8b
Simone Taeggi <startae14@gmail.com>
2024-01-12 23:25:47 +0100

    20 Iteration Map over code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import ContactCard from './ContactCard';
+
+const data = [
+  {
+    id: 'sunita-abc123',
+    name: 'Sunita Kumar',
+    job: 'Electrical Engineer',
+    email: 'sunita.kumar@acme.co',
+  },
+  {
+    id: 'henderson-def456',
+    name: 'Henderson G. Sterling II',
+    job: 'Receptionist',
+    email: 'henderson-the-second@acme.co',
+  },
+  {
+    id: 'aio-ghi789',
+    name: 'Aoi Kobayashi',
+    job: 'President',
+    email: 'kobayashi.aoi@acme.co',
+  },
+]
 
 function App() {
   return (
-    <React.Fragment>
-      <h1>Welcome to my homepage!</h1>
-      <p>Don't forget to sign the guestbook!</p>
-    </React.Fragment>
+    <ul>
+      {
+        data.map(({ id, name, job, email }) => (
+          <ContactCard
+            key={id}
+            name={name}
+            job={job}
+            email={email}
+          />
+        ))
+      }
+    </ul>
   );
 }
 
