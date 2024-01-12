e913cad1e80c49e8cf0cce3c54a64469cd70896c
Simone Taeggi <startae14@gmail.com>
2024-01-12 01:43:36 +0100

    14 Creating a “Button” component exercise

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React from 'react';
 import { createRoot } from 'react-dom/client';
-import "./reset.css";
-import "./styles.css";
-import App from "./App";
 
-const container = document.getElementById('root');
-const root = createRoot(container);
+const root = createRoot(
+  document.querySelector('#root')
+);
 
-root.render(<App />);
+function Button({ children, color }) {
+  return (
+    <button
+      style={{
+        border: '2px solid',
+        color:  color,
+        borderColor: color,
+        background: 'white',
+        borderRadius: 4,
+        padding: 16,
+        margin: 8,
+      }}
+    >
+      {children}
+    </button>
+  )
+}
+
+root.render(
+  <div>
+    <Button color="red">Cancel</Button>
+    <Button color="black">Confirm</Button>
+  </div>
+);
