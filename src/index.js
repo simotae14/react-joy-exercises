262a6a60df63323e1f77a98fa168da8ba0234713
Simone Taeggi <startae14@gmail.com>
2024-01-12 01:21:06 +0100

    09 Basic Syntax Components

--- src/index.js
+++ src/index.js
@@ -1,62 +1,20 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-import { formatDate } from './data-helpers';
-
-const message = {
-  content:
-    'Just ate at “Les Corbeaux En Colère”. Wonderful little venue!',
-  published: '2024-03-14T15:09:26.000Z',
-  author: {
-    avatarSrc: 'https://sandpack-bundler.vercel.app/img/avatars/009.png',
-    avatarDescription: 'Cartoon bear',
-    name: 'Ben Thorn',
-    handle: 'benjaminthorn',
-  }
-};
-
-const element = (
-  <article style={{ filter: 'var(--shadow-elevation-high)' }}>
-    {/*
-      TODO: Stuff here!
-    */}
-    <header>
-      <img 
-        alt={message.author.avatarDescription}
-        src={message.author.avatarSrc}
-      />
-      <a href={`/users/${message.author.handle}`}>{ message.author.name }</a>
-    </header>
-    <p>{message.content}</p>
-    <footer>
-      Posted{' '}
-      <time dateTime={message.published}>
-        { formatDate(message.published) }
-      </time>
-    </footer>
-  </article>
-);
-
-/*
-Here's the raw HTML:
-
-<article style="filter: var(--shadow-elevation-high)">
-  <header>
-    <img src="TODO">
-    <a href="">Author name here</a>
-  </header>
-  <p>
-    Message content here
-  </p>
-  <footer>
-    Posted
-    <time datetime="2024-01-01T00:00:00.000Z">
-      January 1st at 12:00am
-    </time>
-  </footer>
-</article>
-*/
+function FriendlyGreeting() {
+  return (
+    <p
+      style={{
+        fontSize: '1.25rem',
+        textAlign: 'center',
+        color: 'sienna',
+      }}
+    >
+      Greetings, weary traveller!
+    </p>
+  );
+}
 
 const container = document.querySelector('#root');
 const root = createRoot(container);
-root.render(element);
+root.render(<FriendlyGreeting />);
