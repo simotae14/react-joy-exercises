b77187620513df1981c48344b2e66720ebbe1acf
Simone Taeggi <startae14@gmail.com>
2024-01-13 19:48:59 +0100

    38 The useState Hook

--- src/App.js
+++ src/App.js
@@ -1,39 +1,13 @@
 import React from 'react';
 
-import VisuallyHidden from './VisuallyHidden';
+function Counter() {
+  const [count, setCount] = React.useState(0);
 
-function ClickBallGame() {
-  function handleClick(type) {
-    if (type === 'win') {
-      alert('You win!');
-    } else {
-      alert('You lose :(');
-    }
-  }
-  
   return (
-    <div className="wrapper">
-      <button
-        className="ball"
-        onClick={() => handleClick("win")}
-      >
-        <VisuallyHidden>
-          Ball
-        </VisuallyHidden>
-      </button>
-      <button
-        className="bomb"
-        onClick={() => handleClick("lose")}
-      >
-        <span
-          role="img"
-          aria-label="bomb"
-        >
-          💣
-        </span>
-      </button>
-    </div>
+    <button onClick={() => setCount(count + 1)}>
+      Value: {count}
+    </button>
   );
 }
 
-export default ClickBallGame;
+export default Counter;
