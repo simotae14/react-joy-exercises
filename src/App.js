677ffff1aa41ef25cc3b5b0b95d55a40c4bbb7d2
Simone Taeggi <startae14@gmail.com>
2024-01-17 22:34:39 +0100

    55 Complex State - Exercise Gradient Generator

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 function App() {
   const [colors, setColors] = React.useState([
     '#FFD500',
@@ -8,9 +7,44 @@ function App() {
   
   const colorStops = colors.join(', ');
   const backgroundImage = `linear-gradient(${colorStops})`;
-  
+
+  const handleOnRemove = () => {
+    if (colors.length <= 2) {
+      window.alert('There is a minimum of 2 colors');
+      return;
+    }
+    const newColors = [...colors];
+    newColors.pop();
+    setColors(newColors);
+  }
+
+  const handleOnAdd = () => {
+    if (colors.length >= 5) {
+      window.alert('There is a maximum of 5 colors');
+      return;
+    }
+    const newColors = [...colors];
+    newColors.push('#FF0000');
+    setColors(newColors);
+  }
+
+  const handleOnChange = (event, index) => {
+    const newColors = [...colors];
+    newColors[index] = event.target.value;
+    setColors(newColors);
+  }
+
   return (
-    <>
+    <div className="wrapper">
+      <div className="actions">
+        <button onClick={handleOnRemove}>
+          Remove color
+        </button>
+        <button onClick={handleOnAdd}>
+          Add color
+        </button>
+      </div>
+      
       <div
         className="gradient-preview"
         style={{
@@ -18,32 +52,27 @@ function App() {
         }}
       />
       
-      <form>
+      <div className="colors">
         {colors.map((color, index) => {
           const colorId = `color-${index}`;
-          
           return (
-            <div key={colorId} className="color-row">
+            <div key={colorId} className="color-wrapper">
               <label htmlFor={colorId}>
                 Color {index + 1}:
               </label>
-              <input
-                id={colorId}
-                type="color"
-                value={color}
-                onChange={event => {
-                  // fix the mutation bug
-                  const nextColors = [...colors];
-                  nextColors[index] = event.target.value;
-                  
-                  setColors(nextColors);
-                }}
-              />
+              <div className="input-wrapper">
+                <input
+                  id={colorId}
+                  type="color"
+                  value={colors[index]}
+                  onChange={(event) => handleOnChange(event, index)}
+                />
+              </div>
             </div>
           );
         })}
-      </form>
-    </>
+      </div>
+    </div>
   );
 }
 
