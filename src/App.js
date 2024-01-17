319904d0ebd808a370f149f0ffc6f153c65d2846
Simone Taeggi <startae14@gmail.com>
2024-01-17 23:40:06 +0100

    57 Dynamic Key Generation

--- src/App.js
+++ src/App.js
@@ -1,79 +1,11 @@
-import React from 'react';
+import StickerPad from './StickerPad';
+
 function App() {
-  const [colors, setColors] = React.useState([
-    '#FFD500',
-    '#FF0040',
-    '#FF0040',
-    '#FF0040',
-    '#FF0040',
-  ]);
-  const [numOfVisibleColors, setNumOfVisibleColors] = React.useState(2);
-  const visibleColors = colors.slice(0, numOfVisibleColors)
-  const colorStops = visibleColors.join(', ');
-  const backgroundImage = `linear-gradient(${colorStops})`;
-
-  const handleOnRemove = () => {
-    if (numOfVisibleColors <= 2) {
-      window.alert('There is a minimum of 2 colors');
-      return;
-    }
-    
-    setNumOfVisibleColors(numOfVisibleColors - 1);
-  }
-
-  const handleOnAdd = () => {
-    if (numOfVisibleColors >= 5) {
-      window.alert('There is a maximum of 5 colors');
-      return;
-    }
-    setNumOfVisibleColors(numOfVisibleColors + 1);
-  }
-
-  const handleOnChange = (event, index) => {
-    const newColors = [...colors];
-    newColors[index] = event.target.value;
-    setColors(newColors);
-  }
-
   return (
-    <div className="wrapper">
-      <div className="actions">
-        <button onClick={handleOnRemove}>
-          Remove color
-        </button>
-        <button onClick={handleOnAdd}>
-          Add color
-        </button>
-      </div>
-      
-      <div
-        className="gradient-preview"
-        style={{
-          backgroundImage,
-        }}
-      />
-      
-      <div className="colors">
-        {visibleColors.map((color, index) => {
-          const colorId = `color-${index}`;
-          return (
-            <div key={colorId} className="color-wrapper">
-              <label htmlFor={colorId}>
-                Color {index + 1}:
-              </label>
-              <div className="input-wrapper">
-                <input
-                  id={colorId}
-                  type="color"
-                  value={colors[index]}
-                  onChange={(event) => handleOnChange(event, index)}
-                />
-              </div>
-            </div>
-          );
-        })}
-      </div>
-    </div>
+    <>
+      <p>Click around to add stickers!</p>
+      <StickerPad />
+    </>
   );
 }
 
---
+++ src/StickerPad.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import styles from './StickerPad.module.css';
+import { getSticker } from './Stickers.data';
+
+function StickerPad() {
+  const [stickers, setStickers] = React.useState([]);
+
+  return (
+    <button
+      className={styles.wrapper}
+      onClick={(event) => {
+        const stickerData = getSticker();
+        const newSticker = {
+          ...stickerData,
+          x: event.clientX,
+          y: event.clientY,
+          // id: Math.random()
+          id: crypto.randomUUID()
+        };
+
+        const nextStickers = [...stickers, newSticker];
+        setStickers(nextStickers);
+      }}
+    >
+      {stickers.map((sticker) => (
+        <img
+          key={sticker.id}
+          className={styles.sticker}
+          src={sticker.src}
+          alt={sticker.alt}
+          style={{
+            left: sticker.x,
+            top: sticker.y,
+            width: sticker.width,
+            height: sticker.height,
+          }}
+        />
+      ))}
+    </button>
+  );
+}
+
+export default StickerPad;
