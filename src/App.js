77678c4e2fea7aa4fe8534ac2edd5054a256dd3b
Simone Taeggi <startae14@gmail.com>
2024-01-19 21:56:18 +0100

    60 Lifting State Up

--- src/App.js
+++ src/App.js
@@ -1,48 +1,24 @@
 import React from 'react';
 
+import SearchForm from './SearchForm';
+import SearchResults from './SearchResults';
+
 function App() {
-  const [invitees, setInvitees] = React.useState([
-    'J. Jonah Jameson',
-    'Mary Jane',
-    'Aunt May',
-  ]);
-
-  // NOTE: This form is incomplete. It should have:
-  // • A <label> for each input
-  // • A <form> tag, with submission behaviour
-  //
-  // I'm omitting them here because they're not
-  // relevant to the problem at hand. Please don't
-  // use this markup as a template for anything real 😅
-
+  const [searchTerm, setSearchTerm] = React.useState('');
   return (
     <>
-      <h1>Invitees</h1>
-      <ul>
-        {invitees.map((item, index) => (
-          <li key={index}>
-            <input
-              // the `defaultValue` attribute
-              // allows us to initialize the input
-              // to a particular value, without
-              // binding the input to it. This
-              // will produce an *uncontrolled*
-              // input.
-              defaultValue={invitees[index]}
-            />
-            <button
-              onClick={() => {
-                const nextInvitees = [...invitees];
-                nextInvitees.splice(index, 1);
-                
-                setInvitees(nextInvitees);
-              }}
-            >
-              Delete
-            </button>
-          </li>
-        ))}
-      </ul>
+      <header>
+        <a className="logo" href="/">
+          Wanda’s Fruits
+        </a>
+        <SearchForm 
+          searchTerm={searchTerm}
+          setSearchTerm={setSearchTerm}
+        />
+      </header>
+      <main>
+        <SearchResults searchTerm={searchTerm} />
+      </main>
     </>
   );
 }
---
+++ src/SearchResults.js
@@ -0,0 +1,9 @@
+function SearchResults({ searchTerm }) {
+  return (
+    <p>
+      You searched for: {searchTerm}.
+    </p>
+  );
+}
+
+export default SearchResults;
