175c66ca19b1c90220bc13b8b441274f84b06303
Simone Taeggi <startae14@gmail.com>
2024-01-22 11:34:05 +0100

    71 Input Cheatsheet - Range input

--- src/App.js
+++ src/App.js
@@ -1,30 +1,8 @@
 import React from 'react';
 
-// The source of truth!
-const OPTIONS = [
-  {
-    label: '18 and under',
-    value: '0-18'
-  },
-  {
-    label: '19 to 39',
-    value: '19-39'
-  },
-  {
-    label: '40 to 64',
-    value: '40-64'
-  },
-  {
-    label: '65 and over',
-    value: '65-infinity'
-  },
-];
-
 function App() {
-  // Grab the first option from the array.
-  // Set its value into state:
-  const [age, setAge] = React.useState(OPTIONS[0].value);
-
+  const [volume, setVolume] = React.useState(50);
+  
   return (
     <>
       <form
@@ -32,35 +10,24 @@ function App() {
           event.preventDefault();
         }}
       >
-        <label htmlFor="age-select">
-          How old are you?
+        <label htmlFor="volume-slider">
+          Audio volume:
         </label>
-        
-        <select
-          id="age-select"
-          value={age}
+        <input
+          type="range"
+          id="volume-slider"
+          min={0}
+          max={100}
+          value={volume}
           onChange={event => {
-            setAge(event.target.value)
+            setVolume(event.target.value);
           }}
-        >
-          {/*
-            Iterate over that array, to create
-            the <option> tags dynamically:
-          */}
-          {OPTIONS.map(option => (
-            <option
-              key={option.value}
-              value={option.value}
-            >
-              {option.label}
-            </option>
-          ))}
-        </select>
+        />
       </form>
       
       <p>
-        <strong>Selected value:</strong>
-        {age}
+        <strong>Current value:</strong>
+        {volume}
       </p>
     </>
   );
