50146a85107830d61a3b3e42faee4def89985b95
Simone Taeggi <startae14@gmail.com>
2024-01-30 18:15:10 +0100

    76 React Hooks - Hook rules - Exercises - 01 Fix the violation - sol 1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,53 @@
 import React from 'react';
+import { X, Search } from 'react-feather';
 
-import TextInput from './TextInput';
+import VisuallyHidden from './VisuallyHidden';
 
 function App() {
+  const [
+    showSearchField,
+    setShowSearchField,
+  ] = React.useState(false);
+
+  const reactId = React.useId();
+  let searchId = showSearchField ? reactId : undefined;
+  // if (showSearchField) {
+  //   searchId = React.useId();
+  // }
+
+  function handleToggleSearch(event) {
+    event.preventDefault();
+    setShowSearchField(!showSearchField);
+  }
+
   return (
     <>
-      <TextInput label="Name" />
-      <TextInput
-        id="my-email"
-        label="Email"
-        type="email"
-      />
+      <form>
+        {showSearchField && (
+          <div className="search-field-wrapper">
+            <label
+              htmlFor={searchId}
+            >
+              <VisuallyHidden>
+                Search
+              </VisuallyHidden>
+            </label>
+            <input
+              id={searchId}
+              className="search-field"
+            />
+          </div>
+        )}
+        <button
+          className="search-toggle-button"
+          onClick={handleToggleSearch}
+        >
+          {showSearchField ? <X /> : <Search />}
+          <VisuallyHidden>
+            Toggle search field
+          </VisuallyHidden>
+        </button>
+      </form>
     </>
   );
 }
