7a7a4781b70efd3f5374846ad26a7d41da3ecf37
Simone Taeggi <startae14@gmail.com>
2024-01-30 22:20:57 +0100

    78 React Hooks - Hook rules - Exercises - 02 Fix another violation - sol 1

--- src/App.js
+++ src/App.js
@@ -1,60 +1,35 @@
 import React from 'react';
-import { X, Search } from 'react-feather';
 
-import VisuallyHidden from './VisuallyHidden';
+import LoginForm from './LoginForm';
 
 function App() {
-  const [
-    showSearchField,
-    setShowSearchField,
-  ] = React.useState(false);
+  const [isLoggedIn, setIsLoggedIn] = React.useState(false);
 
-  const reactId = React.useId();
-  let searchId = showSearchField ? reactId : undefined;
-  // if (showSearchField) {
-  //   searchId = React.useId();
-  // }
-
-  function handleToggleSearch(event) {
+  function handleLogin(event) {
+    // NOTE: In a real application, we'd perform a
+    // network request here, to validate the login.
+    // We'll see how to do this later in this module.
     event.preventDefault();
-    setShowSearchField(!showSearchField);
+    setIsLoggedIn(true);
   }
 
   return (
     <>
-      <form>
-        {showSearchField && <SearchField />}
+      <LoginForm
+        isLoggedIn={isLoggedIn}
+        handleLogin={handleLogin}
+      />
+      {isLoggedIn && (
         <button
-          className="search-toggle-button"
-          onClick={handleToggleSearch}
+          onClick={(event) => {
+            setIsLoggedIn(false);
+          }}
         >
-          {showSearchField ? <X /> : <Search />}
-          <VisuallyHidden>
-            Toggle search field
-          </VisuallyHidden>
+          Log Out
         </button>
-      </form>
+      )}
     </>
   );
-}
-
-function SearchField() {
-  const searchId = React.useId();
-  return (
-    <div className="search-field-wrapper">
-      <label
-        htmlFor={searchId}
-      >
-        <VisuallyHidden>
-          Search
-        </VisuallyHidden>
-      </label>
-      <input
-        id={searchId}
-        className="search-field"
-      />
-    </div>
-  )
 }
 
 export default App;
---
+++ src/LoginForm.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+function LoginForm({ isLoggedIn, handleLogin }) {
+  const id = React.useId();
+
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  if (isLoggedIn) {
+    return <p>You're already logged in!</p>;
+  }
+
+  return (
+    <form onSubmit={handleLogin}>
+      <div className="row">
+        <label htmlFor={`${id}-email`}>Email:</label>
+        <input
+          type="email"
+          id={`${id}-email`}
+          value={email}
+          onChange={(event) => {
+            setEmail(event.target.value);
+          }}
+        />
+      </div>
+      <div className="row">
+        <label htmlFor={`${id}-password`}>
+          Password:
+        </label>
+        <input
+          type="password"
+          id={`${id}-password`}
+          value={password}
+          onChange={(event) => {
+            setPassword(event.target.value);
+          }}
+        />
+      </div>
+      <button>Log in</button>
+    </form>
+  );
+}
+
+export default LoginForm;
