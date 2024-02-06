d0ab3ce09e3edfc01d704f53e327b855ce0c5609
Simone Taeggi <startae14@gmail.com>
2024-02-06 23:55:48 +0100

    87 Side Effect - Effect Lint Rules

--- src/App.js
+++ src/App.js
@@ -1,44 +1,19 @@
-/*
-Local Storage cheatsheet:
-
-// To save an item:
-window.localStorage.setItem('is-dark-mode', true);
-
-// To retrieve the value:
-JSON.parse(window.localStorage.getItem('is-dark-mode'));
-*/
-
 import React from 'react';
 
-import Toggle from './Toggle';
-
 function App() {
-  const [isDarkMode, setIsDarkMode] = React.useState(() => {
-    // Retrieve the value:
-    const storedValue = JSON.parse(window.localStorage.getItem('is-dark-mode'));
-    return storedValue || false;
-  });
+  const [count, setCount] = React.useState(0);
 
   React.useEffect(() => {
-    window.localStorage.setItem('is-dark-mode', isDarkMode);
-  }, [isDarkMode]);
+    console.log(count);
+  }, [count]);
 
   return (
-    <div
-      className="wrapper"
-      style={{
-        // NOTE: This is a just-for-fun mini demo, not a
-        // full-featured Dark Mode implementation!
-        '--color-bg': isDarkMode ? 'black' : 'white',
-        '--color-text': isDarkMode ? 'white' : 'black',
-      }}
-    >
-      <Toggle
-        label="Dark Mode"
-        checked={isDarkMode}
-        handleToggle={setIsDarkMode}
-      />
-    </div>
+    <>
+      <p>The count is: {count}</p>
+      <button onClick={() => setCount(count + 1)}>
+        Increment
+      </button>
+    </>
   );
 }
 
