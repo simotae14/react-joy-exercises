2d3f2cfcc449de612a37bbd8d157c18bc30f1a88
Simone Taeggi <startae14@gmail.com>
2024-02-14 01:02:24 +0100

    97 Side Effect - Cleanup Functions Exercises - 04 Useless Machine

--- src/App.js
+++ src/App.js
@@ -1,22 +1,50 @@
 import React from 'react';
 
-import Clock from './Clock'
+import VisuallyHidden from './VisuallyHidden';
 
-function App() {
-  const [showClock, setShowClock] = React.useState(true);
+function UselessMachine() {
+  const id = React.useId();
+  const [isOn, setIsOn] = React.useState(true);
+
+  React.useEffect(() => {
+    // ignore renders that happen because the
+    // checkbox is flipped on. We only want to
+    // respond when it's flipped *off*.
+    if (isOn) {
+      return;
+    }
+
+    // Flip the checkbox back after 500ms
+    const timeoutId = window.setTimeout(() => {
+      setIsOn(true);
+    }, 500);
+
+    // cleanup function
+    return () => {
+      // unless the 'isOn' property changes
+      // before the time has elapsed, OR the
+      // component happens to unmount
+      window.clearTimeout(timeoutId);
+    };
+  }, [isOn]);
   
   return (
     <>
-      <button
-        className="clock-toggle"
-        onClick={() => setShowClock(!showClock)}
-      >
-        {showClock ? 'Clock ON' : 'Clock OFF'}
-      </button>
-      
-      {showClock && <Clock />}
+      <input
+        id={id}
+        type="checkbox"
+        checked={isOn}
+        onChange={event => {
+          setIsOn(event.target.checked);
+        }}
+      />
+      <VisuallyHidden>
+        <label htmlFor={id}>
+          Toggle checkbox
+        </label>
+      </VisuallyHidden>
     </>
   );
 }
 
-export default App;
+export default UselessMachine;
