065552d161aca9355159ef6237fb5cd72b030758
Simone Taeggi <startae14@gmail.com>
2024-02-15 18:52:29 +0100

    98 Side Effects - State Values

--- src/App.js
+++ src/App.js
@@ -1,50 +1,15 @@
 import React from 'react';
 
-import VisuallyHidden from './VisuallyHidden';
+import MediaPlayer from './MediaPlayer'
 
-function UselessMachine() {
-  const id = React.useId();
-  const [isOn, setIsOn] = React.useState(true);
+const DEMO_SONG_SRC = 'https://storage.googleapis.com/joshwcomeau/bvrnout-take-it-easy-short.mp3';
 
-  React.useEffect(() => {
-    // ignore renders that happen because the
-    // checkbox is flipped on. We only want to
-    // respond when it's flipped *off*.
-    if (isOn) {
-      return;
-    }
-
-    // Flip the checkbox back after 500ms
-    const timeoutId = window.setTimeout(() => {
-      setIsOn(true);
-    }, 500);
-
-    // cleanup function
-    return () => {
-      // unless the 'isOn' property changes
-      // before the time has elapsed, OR the
-      // component happens to unmount
-      window.clearTimeout(timeoutId);
-    };
-  }, [isOn]);
-  
+function App() {
   return (
     <>
-      <input
-        id={id}
-        type="checkbox"
-        checked={isOn}
-        onChange={event => {
-          setIsOn(event.target.checked);
-        }}
-      />
-      <VisuallyHidden>
-        <label htmlFor={id}>
-          Toggle checkbox
-        </label>
-      </VisuallyHidden>
+      <MediaPlayer src={DEMO_SONG_SRC} />
     </>
   );
 }
 
-export default UselessMachine;
+export default App;
