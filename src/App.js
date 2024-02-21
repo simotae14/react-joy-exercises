d150254a0a4be1b51ffbc065ff1742d9356b7604
Simone Taeggi <startae14@gmail.com>
2024-02-21 22:27:47 +0100

    103 Side Effects - Strict Mode

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 
-import Timer from './Timer'
+import MediaPlayer from './MediaPlayer'
+
+const DEMO_SONG_SRC = 'https://storage.googleapis.com/joshwcomeau/bvrnout-take-it-easy-short.mp3';
 
 function App() {
+  const [
+    showMediaPlayer,
+    setShowMediaPlayer
+  ] = React.useState(true);
+  
   return (
     <>
-      <Timer />
+      <button
+        onClick={() => setShowMediaPlayer(!showMediaPlayer)}
+      >
+        Toggle media player
+      </button>
+      {showMediaPlayer && <MediaPlayer src={DEMO_SONG_SRC} />}
     </>
   );
 }
